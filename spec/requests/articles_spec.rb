379211c7c4246f84fa7ff33374579956ffada1da
Hiroyuki KOBAYASHI <hkob@metro-cit.ac.jp>
2024-02-18 20:43:12 +0900

    所有しないオブジェクトの処理(テストを追加): 小林研 Rails Tips (80)

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe ArticlesController, type: :request do
         it_behaves_like "描画結果に文字列が含まれている?", "記事更新"
       end
 
-      context "when now owned object" do
+      context "when not owned object" do
         let(:one) { not_mine }
         it_behaves_like "レスポンスコード確認", 302
         it_behaves_like "rootリダイレクト確認"
@@ -64,44 +64,50 @@ RSpec.describe ArticlesController, type: :request do
     end
 
     describe "PATCH #update" do
-      subject { -> { patch article_path(object), params: {article: attrs} } }
-      let(:return_path) { article_path(object) }
-      context "正しいパラメータに対して" do
-        before { attrs["title"] = "ABC" }
-        it_behaves_like "レスポンスコード確認", 302
-        it_behaves_like "オブジェクト属性が変化した?", Article, :title, "ABC"
-        it_behaves_like "リダイレクト確認"
-        it_behaves_like "Notice メッセージ確認", "記事を更新しました。"
+      subject { -> { patch article_path(one), params: {article: attrs} } }
+      context "when owned object" do
+        let(:one) { object }
+        let(:return_path) { article_path(object) }
+        context "正しいパラメータに対して" do
+          before { attrs["title"] = "ABC" }
+          it_behaves_like "レスポンスコード確認", 302
+          it_behaves_like "オブジェクト属性が変化した?", Article, :title, "ABC"
+          it_behaves_like "リダイレクト確認"
+          it_behaves_like "Notice メッセージ確認", "記事を更新しました。"
+        end
+
+        context "不正なパラメータに対して" do
+          before { attrs["title"] = "" }
+          it_behaves_like "レスポンスコード確認", 422
+          it_behaves_like "オブジェクト属性が変化しない?", Article, :title
+          it_behaves_like "Alert メッセージ確認", "記事の更新に失敗しました。"
+        end
       end
 
-      context "不正なパラメータに対して" do
-        before { attrs["title"] = "" }
-        it_behaves_like "レスポンスコード確認", 422
-        it_behaves_like "オブジェクト属性が変化しない?", Article, :title
-        it_behaves_like "Alert メッセージ確認", "記事の更新に失敗しました。"
+      context "when not owned object" do
+        let(:one) { not_mine }
+        it_behaves_like "レスポンスコード確認", 302
+        it_behaves_like "rootリダイレクト確認"
       end
     end
 
     describe "DELETE #destroy" do
-      subject { -> { delete article_path(object) } }
-      let(:return_path) { articles_path }
-      it_behaves_like "レスポンスコード確認", 303
-      it_behaves_like "オブジェクトが1減るか?", Article
-      it_behaves_like "リダイレクト確認"
-      it_behaves_like "Notice メッセージ確認", "記事を削除しました。"
+      subject { -> { delete article_path(one) } }
+      context "when owned object" do
+        let(:one) { object }
+        let(:return_path) { articles_path }
+        it_behaves_like "レスポンスコード確認", 303
+        it_behaves_like "オブジェクトが1減るか?", Article
+        it_behaves_like "リダイレクト確認"
+        it_behaves_like "Notice メッセージ確認", "記事を削除しました。"
+      end
+
+      context "when not owned object" do
+        let(:one) { not_mine }
+        it_behaves_like "レスポンスコード確認", 302
+        it_behaves_like "rootリダイレクト確認"
+      end
     end
 
-    #   context "now owned object" do
-    #     let(:one) { not_mine }
-    #     it_behaves_like "レスポンスコード確認", 302
-    #     it_behaves_like "rootリダイレクト確認"
-    #   end
-    # end
-
-    #  describe "GET #index" do
-    #    subject { -> { get articles_path } }
-    #    it_behaves_like "レスポンスコード確認", 302
-    #    it_behaves_like "描画結果に文字列が含まれていない?", %w[表示されない文字列]
-    #  end
   end
 end
